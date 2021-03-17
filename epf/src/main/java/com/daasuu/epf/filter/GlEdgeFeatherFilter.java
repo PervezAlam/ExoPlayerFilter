6f73240f5600ff58cfd33f54a108ac1aa71248d6
Pervez Alam <palam@adobe.com>
2021-03-18 02:50:03 +0530

    Modify shader to work on absolute coordinate
    Previous implementation was taking the coordinate system in normalized form, due to which, all the calculations were based on normalized value. This lead to a problem to keep feather value for both dimensions and calculate based on that. It also didn't give the smooth solution.
    The current implementation resolve this issue by denormalizing the coordinate value and use the Rush type algo without further changes.
    TODO: Do we need to handle divide by zero scenario when the feather size is zero?

--- epf/src/main/java/com/daasuu/epf/filter/GlEdgeFeatherFilter.java
+++ epf/src/main/java/com/daasuu/epf/filter/GlEdgeFeatherFilter.java
@@ -12,22 +12,20 @@ public class GlEdgeFeatherFilter extends GlFilter {
             "precision mediump float;\n" +
             "varying vec2 vTextureCoord;\n" +
             "uniform lowp sampler2D sTexture;\n" +
-            "uniform lowp float featherHeight;\n" +
-            "uniform lowp float featherWidth;\n" +
+            "uniform lowp float featherSize;\n" +
             "uniform lowp float imHeight;\n" +
             "uniform lowp float imWidth;\n" +
             "void main()\n" +
             "{\n" +
-            "    gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
-            "    float left = featherWidth - vTextureCoord.x;\n" +
-            "    float top = featherHeight - vTextureCoord.y;\n" +
-            "    float bottom = featherHeight - (1.0 - vTextureCoord.y);\n" +
-            "    float right = featherWidth - (1.0 - vTextureCoord.x);\n" +
-            "    float lr = max(left, right);\n" +
-            "    float tb = max(top, bottom);\n" +
-            "    float dist = max(lr, tb);\n" +
-            "    float featherSize = lr > tb ? featherWidth : featherHeight;\n" +
+            "    float x = float(imWidth) * vTextureCoord.x;\n" +
+            "    float y = float(imHeight) * vTextureCoord.y;\n" +
+            "    float left = featherSize - x;\n" +
+            "    float top = featherSize - y;\n" +
+            "    float bottom = featherSize - (imHeight - y);\n" +
+            "    float right = featherSize - (imWidth - x);\n" +
+            "    float dist = max(max(left, right), max(top, bottom));\n" +
             "    float edgeFeather = (1.0 - dist / featherSize);\n" +
+            "    gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
             "    if (dist > 0.0) {\n" +
             "        gl_FragColor.r *= edgeFeather;\n" +
             "        gl_FragColor.g *= edgeFeather;\n" +
@@ -37,15 +35,8 @@ public class GlEdgeFeatherFilter extends GlFilter {
 
     // feather height/width is normalized (0.0-1.0) on the basis of the feather size and the image's height / width
     private int featherSize = 0;
-    private float featherWidth;
-    private float featherHeight;
     private float imWidth = 0;
     private float imHeight = 0;
-
-    private void updateFeatherSize() {
-        this.featherHeight = (float) featherSize / (float) imHeight;
-        this.featherWidth = (float) featherSize / (float) imWidth;
-    }
 
     public GlEdgeFeatherFilter() {
         super(DEFAULT_VERTEX_SHADER, EDGE_FEATHER_FRAGMENT_SHADER);
@@ -60,21 +51,20 @@ public class GlEdgeFeatherFilter extends GlFilter {
      * @param featherSize value in pixel
      */
     public void setFeatherSize(int featherSize) {
-        this.featherSize = featherSize;
-        updateFeatherSize();
+        //this.featherSize = featherSize;
     }
 
     @Override
     public void setFrameSize(int width, int height) {
         imWidth = (float) width;
         imHeight = (float) height;
-        updateFeatherSize();
     }
 
     @Override
     public void onDraw() {
-        GLES20.glUniform1f(getHandle("featherHeight"), featherHeight);
-        GLES20.glUniform1f(getHandle("featherWidth"), featherWidth);
+        GLES20.glUniform1f(getHandle("featherSize"), (float)featherSize);
+        GLES20.glUniform1f(getHandle("imWidth"), (float)imWidth);
+        GLES20.glUniform1f(getHandle("imHeight"), (float)imHeight);
     }
 
 }
