2ca677fc7e5ab8ec052986db3b320dbfb1fea3bd
Pervez Alam <palam@adobe.com>
2021-03-25 00:57:09 +0530

    Profile performance, relayout, better media browse
    - Added Media Browser with dynamic permission. TODO: Remove media picker activity + fragments
    - Relayout: The main activity layout is modified to provide much info and a scrollable video view with original size of video (either 4k or 1080p atm)
    - Added load control to fix the memory issue causing the playback stuck issue.
    - Effect profile view and it's access for each kernel
    - Default filter is now Edge Feather

--- sample/src/main/java/com/daasuu/exoplayerfilter/MainActivity.java
+++ sample/src/main/java/com/daasuu/exoplayerfilter/MainActivity.java
@@ -1,25 +1,38 @@
 package com.daasuu.exoplayerfilter;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.daasuu.epf.EPlayerView;
+import com.daasuu.epf.filter.GlFilter;
+import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.EventLogger;
@@ -30,13 +43,23 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final int REQUEST_EXTERNAL_STORAGE = 101;
+    private static final int REQUEST_EXTERNAL_STORAGE_ON_RESUME = 1010;
 
+    private Button mediaPicker;
     private EPlayerView ePlayerView;
     private SimpleExoPlayer player;
     private Button button;
     private TextView analyticsView;
+    private TextView effectProfileInfoView;
     private SeekBar seekBar;
     private PlayerTimer playerTimer;
+    private Uri mediaUri = Uri.parse(Constant.STREAM_URL_MP4_VOD_SHORT);
+    private GlFilter glFilter;
 
 
     @Override
@@ -44,12 +67,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpViews();
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        if (!verifyStoragePermissions(this)) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE_ON_RESUME);
+        }
+
         setUpSimpleExoPlayer();
         setUoGlPlayerView();
         setUpTimer();
@@ -85,6 +115,11 @@ public class MainActivity extends AppCompatActivity {
 
         analyticsView = findViewById(R.id.analyticsView);
 
+        mediaPicker = findViewById(R.id.picker_button);
+        mediaPicker.setOnClickListener(v -> startPicker());
+
+        effectProfileInfoView = findViewById(R.id.tvEffectProfile);
+
         // seek
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -119,7 +154,8 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ePlayerView.setGlFilter(FilterType.createGlFilter(filterTypes.get(position), getApplicationContext()));
+                glFilter = FilterType.createGlFilter(filterTypes.get(position), getApplicationContext());
+                ePlayerView.setGlFilter(glFilter);
             }
         });
     }
@@ -129,21 +165,31 @@ public class MainActivity extends AppCompatActivity {
         // Produces DataSource instances through which media data is loaded.
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "yourApplicationName"));
 
-        Uri uri = Uri.parse(Constant.MEDIA_URL_MP4_4k30FPS_SHORT);
-        Intent intent = getIntent();
-        if (intent.getData() != null) {
-            uri = intent.getData();//DEFAULT_MEDIA_URI;testeer
-        }
-
         // This is the MediaSource representing the media to be played.
-        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(uri);
+        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(mediaUri);
 
         // SimpleExoPlayer
-        player = ExoPlayerFactory.newSimpleInstance(this);
+        DefaultLoadControl loadControl = new DefaultLoadControl.Builder()
+                .setBufferDurationsMs(5000, 5000, 2500, 500)
+                .setTargetBufferBytes(64 * 1024 * 2000 + 200 * 64 * 1024)
+                .setPrioritizeTimeOverSizeThresholds(false)
+                .createDefaultLoadControl();
+
+        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(), loadControl);
         // Prepare the player with the source.
         player.prepare(videoSource);
         player.setPlayWhenReady(true);
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                if (playbackState == Player.STATE_ENDED && glFilter != null) {
+                    runOnUiThread(() -> {
+                        String performanceString = glFilter.getPerformanceData();
+                        effectProfileInfoView.setText(performanceString);
+                    });
+                }
+            }
+        });
 
         player.addAnalyticsListener(new EventLogger(null));
         analyticsView.setTextColor(Color.RED);
@@ -153,10 +199,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void setUoGlPlayerView() {
-        ePlayerView = new EPlayerView(this);
+        ePlayerView = findViewById(R.id.playerView);
         ePlayerView.setSimpleExoPlayer(player);
-        ePlayerView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        ((MovieWrapperView) findViewById(R.id.layout_movie_wrapper)).addView(ePlayerView);
+        glFilter = FilterType.createGlFilter(FilterType.EDGE_FEATHER, getApplicationContext());
+        ePlayerView.setGlFilter(glFilter);
         ePlayerView.onResume();
     }
 
@@ -178,10 +224,59 @@ public class MainActivity extends AppCompatActivity {
         playerTimer.start();
     }
 
+    private void startPicker() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 123);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri selectedVideoUri = data.getData();
+                mediaUri = Uri.parse(getRealPathFromURI(selectedVideoUri));
+            } else {
+                Toast.makeText(this, " No Video Selected", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public boolean verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return permission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_EXTERNAL_STORAGE_ON_RESUME) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("ABHI", " Permission Granted , called from on Resume");
+            } else {
+                android.util.Log.d("ABHISHEK", "Permission not given called from on Resume");
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
 
     private void releasePlayer() {
         ePlayerView.onPause();
-        ((MovieWrapperView) findViewById(R.id.layout_movie_wrapper)).removeAllViews();
+//        ((MovieWrapperView) findViewById(R.id.layout_movie_wrapper)).removeAllViews();
         ePlayerView = null;
         player.stop();
         player.release();
